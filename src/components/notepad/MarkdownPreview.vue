f647c950985480013458021fd41e437b0ad94859
korosenpai <koro.dev404@gmail.com>
2022-07-13 11:34:22 +0200

    added markdown support

---
+++ src/components/notepad/MarkdownPreview.vue
@@ -0,0 +1,28 @@
+<template>
+    <div v-html="markdownToHtml"></div>
+</template>
+
+<script setup>
+// import vueMarkdown from 'vue-markdown'
+import { computed } from '@vue/runtime-core'
+import { marked } from 'marked'
+
+const props = defineProps({
+    notesBody: {
+        type: String,
+        default: ""
+    }
+})
+
+const markdownToHtml = computed(() => {
+    return marked(props.notesBody)
+})
+
+</script>
+
+<style scoped lang="scss">
+.markdown-preview {
+    overflow: auto;
+}
+
+</style>
--- src/components/notepad/Notepad.vue
+++ src/components/notepad/Notepad.vue
@@ -4,19 +4,59 @@
         <input type="text" v-model="notesTitle" class="notes-title" placeholder="New Note" @change="saveNote()">
         <div class="separator"></div>
     </div>
-    <textarea class="notes-body" v-model="notesBody" placeholder="Start writing..." @change="saveNote()"></textarea>
+    
+    <!-- <Splitpanes class="notes-body">
+        <pane>
+            <textarea  v-model="notesBody" placeholder="Start writing..." @change="saveNote()"></textarea>
+        </pane>
+        <pane class="markdown-preview" v-if="MarkdownPreviewIsActive"> {{ notesBody }} </pane>
+    </Splitpanes> -->
+    <!-- <multipane>
+        <textarea  v-model="notesBody" placeholder="Start writing..." @change="saveNote()"></textarea>
+        <multipane-resizer></multipane-resizer>
+        <div class="markdown-preview" v-if="MarkdownPreviewIsActive"> {{ notesBody }} </div>
+    </multipane> -->
+
+    <!-- TODO use teleport to define only once -->
+    <textarea class="notes-body" v-if="!MarkdownPreviewIsActive" v-model="notesBody" placeholder="Start writing..." @change="saveNote()"></textarea>
+
+    <VueSplitView
+        direction="horizontal"
+        a-init="50%"
+        a-min="50px"
+        a-max="70%" 
+        class="notes-body"
+        v-if="MarkdownPreviewIsActive"
+    >
+
+        <template #A id="split-view-notepad-panel">
+            <textarea  v-model="notesBody" placeholder="Start writing..." @change="saveNote()"></textarea>
+        </template>
+
+        <template #B>
+            <MarkdownPreview v-if="MarkdownPreviewIsActive" :notesBody="notesBody" />
+        </template>
+    </VueSplitView>
 
     <Tags />
 </div>
 </template>
 
 <script>
-import Tags from '@/components/notepad/Tags.vue'
 import { getNoteFromId } from '@/functions.js'
+import Tags from '@/components/notepad/Tags.vue'
+import MarkdownPreview from '@/components/notepad/MarkdownPreview.vue'
+
+import VueSplitView from 'vue-split-view'
+import 'vue-split-view/dist/vue3.esm.js'
 
 export default {
     components: {
-        Tags
+        Tags,
+        MarkdownPreview,
+        
+        VueSplitView
+
     },
 
     data() {
@@ -31,7 +71,11 @@ export default {
             return this.$store.getters.selectedNoteId
         },
         tags() {
-            return getNoteFromId(this.$store, this.$store.getters.selectedNoteId).tags
+            return getNoteFromId(this.$store, this.$store.getters.selectedNoteId).tags ?? []
+        },
+        MarkdownPreviewIsActive() {
+            // only return true if sidebar is collapsed and "nomd" is not in tags
+            return this.$store.getters.sidebarCollapsed && !this.tags.includes("nomd")
         }
     },
 
@@ -52,11 +96,11 @@ export default {
 
 <style lang="scss">
 .separator {
-  width: 40%;
-  height: 5px;
-  
-  border-radius: 14px;
-  background-color: white;
+    width: 40%;
+    height: 5px;
+
+    border-radius: 14px;
+    background-color: white;
 }
 
 .notepad {
@@ -94,7 +138,7 @@ export default {
     height: 70%;
 
     padding: 15px 30px;
-    margin: 30px 0 0 0;
+    margin: 30px 0 15px 0;
 
     border-radius: 14px;
 
@@ -111,9 +155,37 @@ export default {
     -moz-box-sizing: border-box;
     box-sizing: border-box;
 
+    textarea {
+        height: 95%;
+        width: 100%;
+        overflow: auto;
+        margin: 0;
+        padding: 0;
+        // padding: 15px 30px;
+        background-color: rgba($color: #000000, $alpha: 0);
+        backdrop-filter: none;
+        border: none;
+        outline: none;
+        resize: none;
+        font-size: 24px;
+
+        color: white;
+
+        -webkit-box-sizing: border-box;
+        -moz-box-sizing: border-box;
+        box-sizing: border-box;
+        
+        
+    }
+
     &::placeholder {
         color: white;
         opacity: 1;  // Firefox 
+    }
+
+    &--vertical > &__splitter {
+        min-width: 7px;
+        background: linear-gradient(90deg, #ccc, #111);
     }
 }
 
