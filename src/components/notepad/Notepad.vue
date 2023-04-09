9ca4365cbe2d9389b4fcd195e08d00206cb5f5d2
korosenpai <koro.dev404@gmail.com>
2023-04-09 23:00:02 +0200

    if check directly in <VueSplitView>

--- src/components/notepad/Notepad.vue
+++ src/components/notepad/Notepad.vue
@@ -22,7 +22,7 @@
         </template>
 
         <template #B>
-            <MarkdownPreview ref="markdownPreview" v-if="MarkdownPreviewIsActive" :notesBody="notesBody" />
+            <MarkdownPreview ref="markdownPreview" :notesBody="notesBody" />
         </template>
     </VueSplitView>
 
